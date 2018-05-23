3858b84022b5e2d161f70a27e0235fdd4525512d
Gagan <gagan_gupta@live.in>
2018-05-23 23:41:08 +0530

    updated with default code

--- app.js
+++ app.js
@@ -1,24 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes');
 const _ = require('lodash');
 
-//console.log(_.isString(true));
-//console.log(_.isString('Gagan'));
-var filteredArray = _.uniq(['Gagan', 'Gagan', 1, 3, 5, 5]);
-console.log(filteredArray);
-
-console.log(_.isString('Gagan'));
-//console.log(notes.addNote());
-//console.log(notes.addNumbers(9, -2));
-//testing fs library
-//fs.appendFileSync('greetings.txt', 'hello world');
-
-//testing os library
-//var user = os.userInfo();
-//console.log(user);
-
-//adding fs and os together
-//fs.appendFileSync('greetings.txt', `Hello ${user.username}! your age is ${notes.age}.`);
+const notes = require('./notes');
--- notes.js
+++ notes.js
@@ -4,9 +4,5 @@ console.log('Starting notes.js');
 module.exports.addNote = () => {
     console.log('add Note');
     return 'New Note';
-}
+};
 
-module.exports.addNumbers = (a, b) => {
-    console.log('Adding');
-    return a + b;
-}
