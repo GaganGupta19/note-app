c46fa23e50b848e6dacb10b24d5ea2ebe4cd7c23
Gagan <gagan_gupta@live.in>
2018-05-23 23:30:05 +0530

    used lodash

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const os = require('os');
 const notes = require('./notes');
 const _ = require('lodash');
 
+//console.log(_.isString(true));
+//console.log(_.isString('Gagan'));
+var filteredArray = _.uniq(['Gagan', 'Gagan', 1, 3, 5, 5]);
+console.log(filteredArray);
+
+console.log(_.isString('Gagan'));
 //console.log(notes.addNote());
-console.log(notes.addNumbers(9, -2));
+//console.log(notes.addNumbers(9, -2));
 //testing fs library
 //fs.appendFileSync('greetings.txt', 'hello world');
 
